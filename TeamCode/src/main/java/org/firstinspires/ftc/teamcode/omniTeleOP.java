22168d9efa507657a90dd74fbc0500c052d73cd4
Henry Rosenberg <henrymrosenberg@gmail.com>
2025-11-19 15:25:34 -0800

    fix indentation errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omniTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omniTeleOP.java
@@ -139,120 +139,44 @@ public class omniTeleOP extends LinearOpMode{
 
         waitForStart();
 
-        while (!isStopRequested() && opModeIsActive()) {
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            // Take whichever value is the most drastic change to use from either controller
+            double y = calcLargestChange(-gamepad1.left_stick_y, -gamepad2.left_stick_y); // Y stick values are reported as inverted by the controller
+            double x = calcLargestChange(gamepad1.left_stick_x, gamepad2.left_stick_x);
+            double rx = calcLargestChange(gamepad1.right_stick_x, gamepad2.right_stick_x);
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.initialize(parameters);
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 
 
-            // Hardware Definitions. Must match names setup in robot configuration in the driver hub. config is created and selected selected with driver hub menu
-            // Drive Motors
-            frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-            backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-            frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-            backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-
-            // Game Element Intake
-            intakeLeft = hardwareMap.get(CRServo.class, "intakeLeft");
-            intakeRight = hardwareMap.get(CRServo.class, "intakeRight");
-
-            // Belts to connect intake and flywheel
-            beltLeft = hardwareMap.get(CRServo.class, "beltLeft");
-            beltRight = hardwareMap.get(CRServo.class, "beltRight");
-            beltVertical = hardwareMap.get(CRServo.class, "beltVertical");
-
-            // Reverse some belts
-            intakeLeft.setDirection(CRServo.Direction.REVERSE);
-            beltLeft.setDirection(CRServo.Direction.REVERSE);
-
-            // Flywheel Motor and Rotation
-            flywheelRotateMotor = hardwareMap.dcMotor.get("rotatShot");
-            flywheelRotateMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            flywheelRotateMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            flywheelRotateMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            flywheelMotor = hardwareMap.dcMotor.get("flywheelMotor");
-            flywheelIntake = hardwareMap.dcMotor.get("flywheelIntake");
-            flywheelAngle = hardwareMap.get(Servo.class, "flywheelAngle");
-
-            // Reverse some of the drive motors depending on physical setup
-            frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            //        flywheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-            // Retrieve the IMU from the hardware map
-            imu = hardwareMap.get(IMU.class, "imu");
-            // Adjust the orientation parameters to match your robot <------------------------------------------------------- IMPORTANT
-            IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                    RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                    RevHubOrientationOnRobot.UsbFacingDirection.UP));
-            // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-            imu.initialize(parameters);
-
-
-            waitForStart();
-
-
-            if (isStopRequested()) return;
-
-            while (opModeIsActive()) {
-                // Take whichever value is the most drastic change to use from either controller
-                double y = calcLargestChange(-gamepad1.left_stick_y, -gamepad2.left_stick_y); // Y stick values are reported as inverted by the controller
-                double x = calcLargestChange(gamepad1.left_stick_x, gamepad2.left_stick_x);
-                double rx = calcLargestChange(gamepad1.right_stick_x, gamepad2.right_stick_x);
-
-                // This button choice was made so that it is hard to hit on accident,
-                // it can be freely changed based on preference.
-                // The equivalent button is start on Xbox-style controllers.
-                if (gamepad1.options) {
-                    imu.initialize(parameters);
-                    imu.resetYaw();
-                }
-
-                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-                // Rotate the movement direction counter to the bot's rotation
-                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-                rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-                // Denominator is the largest motor power (absolute value) or 1
-                // This ensures all the powers maintain the same ratio,
-                // but only if at least one is out of the range [-1, 1]
-                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-
-                double frontLeftPower = (rotY + rotX + rx) / denominator;
-                double backLeftPower = (rotY - rotX + rx) / denominator;
-                double frontRightPower = (rotY - rotX - rx) / denominator;
-                double backRightPower = (rotY + rotX - rx) / denominator;
-
-
-                double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
-                max = Math.max(max, Math.abs(backLeftPower));
-                max = Math.max(max, Math.abs(backRightPower));
-
-                if (max > 1.0) {
-                    frontLeftPower /= max;
-                    frontRightPower /= max;
-                    backLeftPower /= max;
-                    backRightPower /= max;
-                }
-
-                frontLeftMotor.setPower(frontLeftPower);
-                backLeftMotor.setPower(backLeftPower);
-                frontRightMotor.setPower(frontRightPower);
-                backRightMotor.setPower(backRightPower);
-
-                telemetry.addData("vals", "%4.2f, %4.2f, %4.2f", y, x, rx);
-                telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
-                telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
-
-
-                // Controller 1 Intake
-                if (gamepad1.aWasPressed() || gamepad2.aWasPressed()) {
-                    if (intakeToggle) {
-                        intakeToggle = false;
-                    } else {
-                        intakeToggle = true;
-                    }
-                }
+            double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+            max = Math.max(max, Math.abs(backLeftPower));
+            max = Math.max(max, Math.abs(backRightPower));
 
             if (max > 1.0) {
                 frontLeftPower /= max;
@@ -273,11 +197,29 @@ public class omniTeleOP extends LinearOpMode{
 
             // Controller 1 Intake
             if (gamepad1.aWasPressed() || gamepad2.aWasPressed()) {
-                if (intakeToggle) {
-                    intakeToggle = false;
-                } else {
-                    intakeToggle = true;
-                }
+                intakeToggle = !intakeToggle;
+            }
+
+            if (max > 1.0) {
+                frontLeftPower /= max;
+                frontRightPower /= max;
+                backLeftPower /= max;
+                backRightPower /= max;
+            }
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+            telemetry.addData("vals", "%4.2f, %4.2f, %4.2f", y, x, rx);
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+
+
+            // Controller 1 Intake
+            if (gamepad1.aWasPressed() || gamepad2.aWasPressed()) {
+                intakeToggle = !intakeToggle;
             }
 
             if (intakeToggle) {
