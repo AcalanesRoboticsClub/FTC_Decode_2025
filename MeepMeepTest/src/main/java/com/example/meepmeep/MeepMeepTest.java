617f92ab2f9a0031f9c1e21d170be01278719bc6
Henry Rosenberg <henrymrosenberg@gmail.com>
2025-11-30 15:50:55 -0800

    Initial MeepMeep Setup!

---
+++ MeepMeepTest/src/main/java/com/example/meepmeep/MeepMeepTest.java
@@ -0,0 +1,37 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class MeepMeepTest {
+    public static void main(String[] args) throws IOException {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                        .forward(30)
+                        .turn(Math.toRadians(90))
+                        .forward(30)
+                        .turn(Math.toRadians(90))
+                        .forward(30)
+                        .turn(Math.toRadians(90))
+                        .forward(30)
+                        .turn(Math.toRadians(90))
+                        .build());
+
+
+        meepMeep.setBackground(ImageIO.read(MeepMeepTest.class.getClassLoader().getResourceAsStream("decodeField.png")))
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
